f32f52bfbf12afb09a7b561ea25534634432b91e
mindsflee <61688412+mindsflee@users.noreply.github.com>
2026-09-21 12:40:50 +0200

    Update SEC-DNSDynamicUpdates.ps1

--- Modules/SEC-DNSDynamicUpdates.ps1
+++ Modules/SEC-DNSDynamicUpdates.ps1
@@ -1,6 +1,6 @@
 Register-WFLModule `
     -Name "SEC-DNSDynamicUpdates" `
-    -Category "Local" `
+    -Category "Active Directory" `
     -Type "Check" `
     -MITRE "T1557" `
     -Tactic "Credential Access" `
@@ -290,7 +290,7 @@ else {
                 Add-WFLFinding `
                     -Title "DNS dynamic update review incomplete" `
                     -Severity "Low" `
-                    -Category "Local" `
+                    -Category "Active Directory" `
                     -MITRE "T1557" `
                     -Tactic "Credential Access" `
                     -Source "SEC-DNSDynamicUpdates" `
@@ -304,7 +304,7 @@ else {
                 Add-WFLFinding `
                     -Title "DNS dynamic update security review passed" `
                     -Severity "Info" `
-                    -Category "Local" `
+                    -Category "Active Directory" `
                     -MITRE "T1557" `
                     -Tactic "Credential Access" `
                     -Source "SEC-DNSDynamicUpdates" `
@@ -331,7 +331,7 @@ else {
             Add-WFLFinding `
                 -Title "DNS zones accept non-secure dynamic updates" `
                 -Severity "High" `
-                -Category "Local" `
+                -Category "Active Directory" `
                 -MITRE "T1557" `
                 -Tactic "Credential Access" `
                 -Source "SEC-DNSDynamicUpdates" `
@@ -343,7 +343,7 @@ else {
             Add-WFLFinding `
                 -Title "DNS dynamic update review failed" `
                 -Severity "Info" `
-                -Category "Local" `
+                -Category "Active Directory" `
                 -MITRE "T1557" `
                 -Tactic "Credential Access" `
                 -Source "SEC-DNSDynamicUpdates" `
