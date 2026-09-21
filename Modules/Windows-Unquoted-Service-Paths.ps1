ddb5d56d29ceebd71dc242b82b6884232d40f438
mindsflee <61688412+mindsflee@users.noreply.github.com>
2026-09-21 12:22:50 +0200

    Update Windows-Unquoted-Service-Paths.ps1

--- Modules/Windows-Unquoted-Service-Paths.ps1
+++ Modules/Windows-Unquoted-Service-Paths.ps1
@@ -1,6 +1,6 @@
 ﻿Register-WFLModule `
     -Name "Windows-Unquoted-Service-Paths" `
-    -Category "Windows Security" `
+    -Category "Local" `
     -Type "Check" `
     -MITRE "T1574.009" `
     -Tactic "Privilege Escalation" `
@@ -28,7 +28,7 @@ Write-Host "[+] Service path for $serviceName corrected with quotes." -Foregroun
             Add-WFLFinding `
                 -Title "Service review unavailable" `
                 -Severity "Info" `
-                -Category "Windows Security" `
+                -Category "Local" `
                 -MITRE "T1574.009" `
                 -Tactic "Privilege Escalation" `
                 -Source "Windows-Unquoted-Service-Paths" `
@@ -153,7 +153,7 @@ Write-Host "[+] Service path for $serviceName corrected with quotes." -Foregroun
             Add-WFLFinding `
                 -Title "Unquoted service path review passed" `
                 -Severity "Info" `
-                -Category "Windows Services" `
+                -Category "Local" `
                 -MITRE "T1574.009" `
                 -Tactic "Privilege Escalation" `
                 -Source "Windows-Unquoted-Service-Paths" `
@@ -175,7 +175,7 @@ Write-Host "[+] Service path for $serviceName corrected with quotes." -Foregroun
         Add-WFLFinding `
             -Title "Potentially exploitable unquoted service paths detected" `
             -Severity $OverallSeverity `
-            -Category "Windows Security" `
+            -Category "Local" `
             -MITRE "T1574.009" `
             -Tactic "Privilege Escalation" `
             -Source "Windows-Unquoted-Service-Paths" `
